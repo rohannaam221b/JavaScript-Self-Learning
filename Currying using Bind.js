4aec61138153ad8ef57cc584904385f4d8d7b2df
rohannaam221b <83057789+rohannaam221b@users.noreply.github.com>
2021-07-08 12:43:23 +0530

    Add files via upload

---
+++ Currying using Bind.js
@@ -0,0 +1,20 @@
+// //Currying using Bind
+
+let multiply = function (x, y) {
+  console.log(x * y)
+}
+
+let multiplyByTwo = multiply.bind(this, 2)
+multiplyByTwo(3)
+let multiplyByThree = multiply.bind(this, 3)
+multiplyByTwo(10)
+
+// currying using Closure
+
+let multiply = function (x) {
+  return function (y) {
+    console.log(x * y)
+  }
+}
+let multiplyByTwo = multiply(2)
+multiplyByTwo(3)
